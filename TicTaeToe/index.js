f58844d891717d167b20aa5b53b147d387c965a1
Ilyas <ilyasshaikh987654@gmail.com>
2022-05-13 12:55:54 +0530

    Hi this is Tic tac Toe game

---
+++ TicTaeToe/index.js
@@ -0,0 +1,66 @@
+console.log("Welcome to tic tat");
+let music = new Audio("music.mp3")
+let Audioturn = new Audio("gameover.mp3")
+let gameover = new Audio("gameover.mp3")
+let Gameover = false;
+let turn = "X"
+
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
+
+}
+//Function to check win
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && boxtext[e[0]].innerText !== "") {
+            document.querySelector('.info').innerHTML = boxtext[e[0]].innerHTML + " Won"
+            Gameover = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            // document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+        }
+    })
+
+}
+
+//main game logic
+// music.play()
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            Audioturn.play();
+            checkWin();
+            if (!Gameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for" + turn
+
+
+            }
+
+        }
+    })
+})
+reset.addEventListener('click', () => {
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X";
+    Gameover = false
+    document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
+
+})
